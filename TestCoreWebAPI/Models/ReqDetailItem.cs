051d77d2540c8457e0d7eddf6b58d7e6d1d77c6b
Saamer Mansoor <i@saamer.me>
2017-07-06 10:48:27 -0600

    Proper ReqDetailItem Object formatting

--- TestCoreWebAPI/Models/ReqDetailItem.cs
+++ TestCoreWebAPI/Models/ReqDetailItem.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TestCoreWebAPI.Models
 {
     public class ReqDetailItem
     {
-		public long Id { get; set; }
-		public string Name { get; set; }
-		public bool IsComplete { get; set; }    
-    }
+		public long Id { get; set; }            //ProjectNo
+		public string Name { get; set; }        //ProjectDesc
+        public bool IsComplete { get; set; }    //Approved
+		//public string ProjectName { get; set; }  //
+        public string BillTo { get; set; }  //
+		public string ReportTo { get; set; }  //
+		//public string AEName { get; set; }  //
+		public string JDECode { get; set; }  //
+		public string PercentOfProject { get; set; }  //
+		public IList<LoginsAssociated> loginsAssociated { get; set; }
+		public IList<ListOfAE> listOfAEs { get; set; }		public string Value { get; set; }  //
+		public string RequestDate { get; set; }  //
+		public string NewAEName { get; set; }  //
+		public string PercOfPrjRequested { get; set; }  //
+		public string AEComments { get; set; }  //
+		public class LoginsAssociated
+		{
+			public int loginNumber { get; set; }
+		}
+
+		public class ListOfAE
+		{
+			public string aeName { get; set; }
+		}
+	}
 }
